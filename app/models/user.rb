73801a6cc8e3f75a69c34e653811c435f7004736
Grzegorz Zaręba <mobile443@gmail.com>
2018-04-20 14:27:51 +0200

    Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,20 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
   validates :first_name, :last_name, presence: true
 
+  mount_uploader :avatar, AvatarUploader
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    user ||= User.create(first_name: data['first_name'],
-                         last_name: data['last_name'],
-                         email: data['email'],
-                         password: Devise.friendly_token[0, 20])
+    user ||= User.create(
+        remote_avatar_url: data['image'],
+        first_name: data['first_name'],
+        last_name: data['last_name'],
+        email: data['email'],
+        password: Devise.friendly_token[0, 20])
     user
   end
 end
+
+
